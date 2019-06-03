d3ee55b8507c32926d4709ecaa866d1edc10792d
captainTao <578628438@qq.com>
2019-06-04 00:45:06 +0800

    add the home-swiper component

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,19 +1,22 @@
 <template>
   <div class="home">
-    <my-nav></my-nav>
+    <home-header></home-header>
+    <home-swiper></home-swiper>
     <router-link to="/list">Go to List</router-link>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
-import myNav from '@/views/home/nav.vue'
+// import HelloWorld from '@/components/HelloWorld.vue'
+import homeHeader from '@/views/home/nav.vue'
+import homeSwiper from '@/views/home/swiper.vue'
 export default {
   name: 'home',
   components: {
-    HelloWorld,
-    myNav
+    // HelloWorld,
+    homeHeader,
+    homeSwiper
   }
 }
 </script>
---
+++ src/views/home/swiper.vue
@@ -0,0 +1,48 @@
+<template>
+	<div class="wrapper">
+		<swiper :options="swiperOption">
+			<swiper-slide v-for="item of dataList" :key='item.id'>
+				<img class='swiper-img' :src="item.imgUrl">
+			</swiper-slide>
+			<div class="swiper-pagination"  slot="pagination"></div>
+		</swiper>
+	</div>
+</template>
+
+<script>
+export default{
+	name: 'homeSwiper',
+	data(){
+		return{
+			swiperOption:{
+				pagination:'.swiper-pagination',
+				loop: true
+			},
+			dataList:[{
+				id:'001',
+				imgUrl:'http://mp-piao-admincp.qunarzz.com/mp_piao_admin_mp_piao_admin/admin/20195/fe8d03b9f1d5b7391b0c882ca519f279.jpg_750x200_1cc2aa0f.jpg'
+			},{
+				id:'002',
+				imgUrl:'http://mp-piao-admincp.qunarzz.com/mp_piao_admin_mp_piao_admin/admin/20196/b2816bed3f46772d0ba57decd833bc0a.jpg_750x200_0517517a.jpg'
+			},{
+				id:'003',
+				imgUrl:'http://img1.qunarzz.com/piao/fusion/1807/5e/cfee2e9e2f357202.jpg_750x200_b0308a43.jpg'
+			}]
+		}
+	}
+}
+</script>
+
+<style lang="stylus" scoped>
+.wrapper >>> .swiper-pagination-bullet-active
+	background: #fff !important
+.wrapper
+	width: 100%
+	height: 0
+	padding-bottom: 26.67%
+	overflow: hidden
+	background: #eee
+.swiper-img
+	width: 100%
+
+</style>
