9244cba8225e18e915f2db4c46e119b2b7caffdf
captainTao <578628438@qq.com>
2019-06-09 17:26:35 +0800

    add the home-rec component

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -3,23 +3,25 @@
     <home-header></home-header>
     <home-swiper></home-swiper>
     <home-icons></home-icons>
+    <home-rec></home-rec>
     <router-link to="/list">Go to List</router-link>
   </div>
 </template>
 
 <script>
 
-// import HelloWorld from '@/components/HelloWorld.vue'
 import homeHeader from '@/views/home/nav.vue'
 import homeSwiper from '@/views/home/swiper.vue'
 import homeIcons from '@/views/home/icons.vue'
+import homeRec from '@/views/home/rec.vue'
 export default {
   name: 'home',
   components: {
     // HelloWorld,
     homeHeader,
     homeSwiper,
-    homeIcons
+    homeIcons,
+    homeRec
   }
 }
 </script>
---
+++ src/views/home/rec.vue
@@ -0,0 +1,23 @@
+<template>
+	<div class="rec">
+		<div class="subrec border-right"><span class="iconfont">&#xe618;</span>定位失败</div>
+		<div class="subrec border-left"><span class="iconfont">&#xe629;</span>必游榜单</div>
+	</div>
+</template>
+
+<script>
+export default{
+	name:'rec'
+}
+</script>
+<style lang = 'stylus' scoped>
+.rec
+	display: flex
+	height: 1rem
+	margin-bottom: .2rem
+.subrec
+	flex: 1
+	line-height: 1rem
+	text-align: center
+	color: #333
+</style>
